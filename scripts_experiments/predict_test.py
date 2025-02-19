4e21749dfd5b17b0f793e53eb6e2d4ec825acdd9
Eduardo Aguilar <earguila00@hotmail.com>
2025-02-19 16:25:59 +0000

    Change to take out opt and make compatible with separated arguments

--- scripts_experiments/predict_test.py
+++ scripts_experiments/predict_test.py
@@ -1,128 +1,126 @@
 import os
 import sys
+from pathlib import Path
+
 import joblib
+import pandas as pd
 import torch
 from torch_geometric.loader import DataLoader
-import pandas as pd
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(parent_dir)
 
-from utils.utils_model import tml_report, network_outer_report, network_report, calculate_morgan_fingerprints
+from hcatgnet.utils.utils_model import (
+    tml_report,
+)
+from hcatgnet.services.model_report import network_outer_report, network_report
 from options.base_options import BaseOptions
-from data.rhcaa import rhcaa_diene
-from data.biaryl import rhcaa_biaryl
-from data.general_reaction import reaction_representation
-from data.hypervalent_iodine import hypervalent_graph
 
 
-from icecream import ic
+def predict_final_test(
+        graph_dataset,
+        handcrafted_descriptors_dataset,
+        descriptors,
+        folds: int = 10,
+        GNN_experiment_path: Path = Path('results'),
+        TML_experiment_path: Path = Path('results'),
+        GNN_log_dir: Path = Path('results'),
+        TML_log_dir: Path = Path('results')) -> None:
 
-def predict_final_test(opt) -> None:
-
-
-    # Get the current working directory
-    current_dir = os.getcwd()
-    
-    # Load the final test set
-    # Create the dataset
-    if opt.filename_final_test =='biaryl.csv':
-        data = rhcaa_biaryl(opt, opt.filename_final_test, opt.mol_cols, opt.root_final_test, include_fold=False)
-    elif opt.filename_final_test == 'N_S_acetal.csv' or opt.filename == 'asym_hydrogenation.csv':
-        data = reaction_representation(opt, opt.filename_final_test, opt.mol_cols, opt.root_final_test, include_fold=False)
-    elif opt.filename_final_test == 'hypervalent_iodine.csv':
-        data = hypervalent_graph(opt, opt.filename_final_test, opt.mol_cols, opt.root_final_test, include_fold=False)
-    else:
-        data = rhcaa_diene(opt, opt.filename_final_test, opt.mol_cols, opt.root_final_test, include_fold=False)
-
-    test_loader = DataLoader(data, shuffle=False)
-
-    # Load the data for tml
-    test_set = pd.read_csv(f'{opt.root_final_test}/raw/{opt.filename_final_test}')
-
-    if opt.descriptors == 'bespoke':
-        descriptors = ['LVR1', 'LVR2', 'LVR3', 'LVR4', 'LVR5', 'LVR6', 'LVR7', 'VB', 'ER1', 'ER2', 'ER3', 'ER4', 'ER5', 'ER6',
-                'ER7', 'SStoutR1', 'SStoutR2', 'SStoutR3', 'SStoutR4', 'temp']
-    elif opt.descriptors == 'morgan':
-        fingerprints = calculate_morgan_fingerprints(df = test_set, smiles_cols=opt.mol_cols, variance_threshold=0)
-        test_set = pd.concat([test_set, fingerprints], axis = 1)
-    elif opt.descriptors == 'circus_fp':
-        test_set = test_set[opt.mol_cols + ['temp', 'ddG', 'index']]
-        if opt.filename == 'biaryl.csv':
-            fingerprints = pd.read_csv('data/datasets/circus_descriptors/biaryl_circus_descriptors.csv')
-        else:
-            fingerprints = pd.read_csv('data/datasets/circus_descriptors/diene_circus_descriptors.csv')
-        test_set = test_set.drop(opt.mol_cols, axis=1)
-        descriptors = ['temp'] + fingerprints.columns.tolist()
-        test_set = pd.merge(test_set, fingerprints, left_index=True, right_index=True)
-        
+    test_loader = DataLoader(graph_dataset, shuffle=False)
 
 
-    experiments_gnn = os.path.join(current_dir, opt.log_dir_results, opt.filename_final_test[:-4], 'test', 'results_GNN')
-    experiments_tml = os.path.join(current_dir, opt.log_dir_results, opt.filename_final_test[:-4], 'test', 'results_TML', opt.tml_algorithm, opt.descriptors)
-
-    for outer in range(1, opt.folds+1):
-        print('Analysing models trained using as test set {}'.format(outer))
-        for inner in range(1, opt.folds):
-    
-            real_inner = inner +1 if outer <= inner else inner
-            
-            print('Analysing models trained using as validation set {}'.format(real_inner))
-
-            model_dir = os.path.join(current_dir, opt.log_dir_results, opt.filename[:-4], 'learning','results_GNN', f'Fold_{outer}_test_set', f'Fold_{real_inner}_val_set')
-
-            model = torch.load(model_dir+'/model.pth', weights_only=False)
-            model_params = torch.load(model_dir+'/model_params.pth', weights_only=True)
-            train_loader = torch.load(model_dir+'/train_loader.pth', weights_only=False)
-            val_loader = torch.load(model_dir+'/val_loader.pth', weights_only=False)
-
-            network_report(log_dir=experiments_gnn,
-                           loaders=(train_loader, val_loader, test_loader),
-                           outer=outer,
-                           inner=real_inner,
-                           loss_lists=[None, None, None],
-                           model=model,
-                           model_params=model_params,
-                           best_epoch=None,
-                           save_all=False)
-            
-            tml_dir = os.path.join(current_dir, opt.log_dir_results, opt.filename[:-4], 'learning', 'results_TML', opt.tml_algorithm, opt.descriptors, f'Fold_{outer}_test_set', f'Fold_{real_inner}_val_set')
-
-            model = joblib.load(tml_dir+'/model.sav')
-            train_data = pd.read_csv(tml_dir+'/train.csv')
-            val_data = pd.read_csv(tml_dir+'/val.csv')
-
-            if opt.descriptors == 'morgan':
-                descriptors = pd.read_csv(tml_dir+'/train.csv').columns.tolist()
-                del descriptors[0]
-                descriptors.remove('ddG')
-                descriptors.remove('fold')
-                descriptors.remove('index')
-
-                new_columns = {descriptor: [0] * len(test_set) for descriptor in descriptors if descriptor not in test_set.columns}
-                fps = pd.DataFrame(new_columns, index = test_set.index)
-                test_set = pd.concat([test_set, fps], axis = 1)
+    # elif representation == "morgan":
+    #     fingerprints = calculate_morgan_fingerprints(
+    #         df=test_set, smiles_cols=opt.mol_cols, variance_threshold=0
+    #     )
+    #     test_set = pd.concat([test_set, fingerprints], axis=1)
+    # elif representation == "circus_fp":
+    #     test_set = test_set[opt.mol_cols + ["temp", "ddG", "index"]]
+    #     if opt.filename == "biaryl.csv":
+    #         fingerprints = pd.read_csv(
+    #             "data/datasets/circus_descriptors/biaryl_circus_descriptors.csv"
+    #         )
+    #     else:
+    #         fingerprints = pd.read_csv(
+    #             "data/datasets/circus_descriptors/diene_circus_descriptors.csv"
+    #         )
+    #     test_set = test_set.drop(opt.mol_cols, axis=1)
+    #     descriptors = ["temp"] + fingerprints.columns.tolist()
+    #     test_set = pd.merge(test_set, fingerprints, left_index=True, right_index=True)
 
 
+    for outer in range(1, folds + 1):
+        print("Analysing models trained using as test set {}".format(outer))
+        for inner in range(1, folds):
 
-            tml_report(log_dir=experiments_tml,
-                       outer=outer,
-                       inner=real_inner,
-                       model=model,
-                       data=(train_data,val_data,test_set),
-                       save_all=False,
-                       descriptors=descriptors)
-            
-                        
-        network_outer_report(log_dir=f"{experiments_gnn}/Fold_{outer}_test_set/", 
-                             outer=outer,
-                             folds=opt.folds)
-        
-        network_outer_report(log_dir=f"{experiments_tml}/Fold_{outer}_test_set/", 
-                             outer=outer,
-                             folds=opt.folds)
-        
-if __name__ == '__main__':
+            real_inner = inner + 1 if outer <= inner else inner
+
+            print(
+                "Analysing models trained using as validation set {}".format(real_inner)
+            )
+
+            model_dir = (
+                GNN_experiment_path
+                / f"Fold_{outer}_test_set"
+                / f"Fold_{real_inner}_val_set"
+            )
+
+            model = torch.load(model_dir / "model.pth", weights_only=False)
+            model_params = torch.load(
+                model_dir / "model_params.pth", weights_only=True
+            )
+            train_loader = torch.load(
+                model_dir / "train_loader.pth", weights_only=False
+            )
+            val_loader = torch.load(model_dir / "val_loader.pth", weights_only=False)
+
+            network_report(
+                log_dir=GNN_log_dir,
+                loaders=(train_loader, val_loader, test_loader),
+                outer=outer,
+                inner=real_inner,
+                loss_lists=[None, None, None],
+                model=model,
+                model_params=model_params,
+                best_epoch=None,
+                save_all=False,
+            )
+
+            tml_dir = (
+                TML_experiment_path
+                / f"Fold_{outer}_test_set"
+                / f"Fold_{real_inner}_val_set"
+            )
+
+            model = joblib.load(tml_dir / "model.sav")
+            train_data = pd.read_csv(tml_dir / "train.csv")
+            val_data = pd.read_csv(tml_dir / "val.csv")
+
+            tml_report(
+                log_dir=TML_log_dir,
+                outer=outer,
+                inner=real_inner,
+                model=model,
+                data=(train_data, val_data, handcrafted_descriptors_dataset),
+                save_all=False,
+                descriptors=descriptors,
+            )
+
+        network_outer_report(
+            log_dir= GNN_log_dir / f'Fold_{outer}_test_set',
+            outer=outer,
+            folds=folds,
+        )
+
+        network_outer_report(
+            log_dir= TML_log_dir / f'Fold_{outer}_test_set/',
+            outer=outer,
+            folds=folds,
+        )
+
+
+if __name__ == "__main__":
     opt = BaseOptions().parse()
     predict_final_test(opt)
--- scripts_experiments/train_TML.py
+++ scripts_experiments/train_TML.py
@@ -1,127 +1,159 @@
 import os
 import sys
+from math import sqrt
+from pathlib import Path
+
 import pandas as pd
 from sklearn.metrics import mean_squared_error
-from math import sqrt
+
+from hcatgnet.services.model_report import network_outer_report
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(parent_dir)
 
-from utils.utils_model import choose_model, hyperparam_tune, load_variables, \
-    split_data, tml_report, network_outer_report, calculate_morgan_fingerprints
+from hcatgnet.utils.utils_model import (
+    calculate_morgan_fingerprints,
+    choose_model,
+    hyperparam_tune,
+    load_variables,
+    split_data,
+    tml_report,
+)
 from options.base_options import BaseOptions
 
 
-def train_tml_model_nested_cv(opt) -> None:
+def train_tml_model_nested_cv(
+    data_csv: pd.DataFrame,
+    mol_cols: list,
+    descriptors: list,
+    tml_algorithm: str = "rf",
+    representation: str = "bespoke",
+    folds: int = 10,
+    log_results_dir: Path = Path("results"),
+    global_seed: int = 20232023,
+) -> None:
 
-    print('Initialising chiral ligands selectivity prediction using a traditional ML approach.')
+    print(
+        "Initialising chiral ligands selectivity prediction using a traditional ML approach."
+    )
 
-    # Load the data
-    filename = opt.filename[:-4] + '_folds' + opt.filename[-4:]
-    data = pd.read_csv(f'{opt.root}/raw/{filename}')
+    if representation == "morgan":
+        data_csv = data_csv[mol_cols + ["temp", "ddG", "fold", "index"]]
+        fingerprints = calculate_morgan_fingerprints(
+            df=data_csv, smiles_cols=mol_cols, variance_threshold=0.01
+        )
+        data_csv = data_csv.drop(mol_cols, axis=1)
+        descriptors = ["temp"] + fingerprints.columns.tolist()
+        print(f"Using {len(descriptors)} fingerprints")
+        data_csv = pd.concat([data_csv, fingerprints], axis=1)
 
-    if opt.descriptors == 'bespoke':
-        data = data[['LVR1', 'LVR2', 'LVR3', 'LVR4', 'LVR5', 'LVR6', 'LVR7', 'VB', 'ER1', 'ER2', 'ER3', 'ER4', 'ER5', 'ER6',
-                'ER7', 'SStoutR1', 'SStoutR2', 'SStoutR3', 'SStoutR4', 'temp', 'ddG', 'fold', 'index']]
-        descriptors = ['LVR1', 'LVR2', 'LVR3', 'LVR4', 'LVR5', 'LVR6', 'LVR7', 'VB', 'ER1', 'ER2', 'ER3', 'ER4', 'ER5', 'ER6',
-                'ER7', 'SStoutR1', 'SStoutR2', 'SStoutR3', 'SStoutR4', 'temp']
-        
-    elif opt.descriptors == 'morgan':
-        data = data[opt.mol_cols + ['temp', 'ddG', 'fold', 'index']]
-        fingerprints = calculate_morgan_fingerprints(df = data, smiles_cols=opt.mol_cols, variance_threshold=.01)
-        data = data.drop(opt.mol_cols, axis=1)
-        descriptors = ['temp'] + fingerprints.columns.tolist() 
-        print(f'Using {len(descriptors)} fingerprints')
-        data = pd.concat([data, fingerprints], axis = 1)
-    
-    elif opt.descriptors == 'circus_fp':
-        data = data[opt.mol_cols + ['temp', 'ddG', 'fold', 'index']]
-        if opt.filename == 'biaryl.csv':
-            fingerprints = pd.read_csv('data/datasets/circus_descriptors/biaryl_circus_descriptors.csv')
+    elif representation == "circus_fp":
+        data_csv = data_csv[mol_cols + ["temp", "ddG", "fold", "index"]]
+        if opt.filename == "biaryl.csv":
+            fingerprints = pd.read_csv(
+                "data/datasets/circus_descriptors/biaryl_circus_descriptors.csv"
+            )
         else:
-            fingerprints = pd.read_csv('data/datasets/circus_descriptors/diene_circus_descriptors.csv')
-        data = data.drop(opt.mol_cols, axis=1)
-        descriptors = ['temp'] + fingerprints.columns.tolist()
-        print(f'Using {len(descriptors)} fingerprints')
-        data = pd.merge(data, fingerprints, left_index=True, right_index=True)
+            fingerprints = pd.read_csv(
+                "data/datasets/circus_descriptors/diene_circus_descriptors.csv"
+            )
+        data_csv = data_csv.drop(mol_cols, axis=1)
+        descriptors = ["temp"] + fingerprints.columns.tolist()
+        print(f"Using {len(descriptors)} fingerprints")
+        data_csv = pd.merge(data_csv, fingerprints, left_index=True, right_index=True)
 
     else:
-        raise ValueError('Descriptors not recognised. Please choose between bespoke or morgan')
+        pass
 
-    
     # Nested cross validation
-    ncv_iterator = split_data(data)
+    ncv_iterator = split_data(data_csv)
 
     # Initiate the counter of the total runs and the total number of runs
     counter = 0
-    TOT_RUNS = opt.folds*(opt.folds-1)    
-    print("Number of splits: {}".format(opt.folds))
+    TOT_RUNS = folds * (folds - 1)
+    print("Number of splits: {}".format(folds))
     print("Total number of runs: {}".format(TOT_RUNS))
 
     # Hyperparameter optimisation
     print("Hyperparameter optimisation starting...")
-    X, y, _ = load_variables(data=data, descriptors=descriptors+['ddG'])
-    best_params = hyperparam_tune(X, y, choose_model(best_params=None, algorithm = opt.tml_algorithm), opt.global_seed)
-    print('Hyperparameter optimisation has finalised')
+    X, y, _ = load_variables(data=data_csv, descriptors=descriptors + ["ddG"])
+    best_params = hyperparam_tune(
+        X,
+        y,
+        choose_model(best_params=None, algorithm=tml_algorithm),
+        global_seed,
+    )
+    print("Hyperparameter optimisation has finalised")
     print("Training starting...")
     print("********************************")
-    
+
     # Loop through the nested cross validation iterators
     # The outer loop is for the outer fold or test fold
-    for outer in range(1, opt.folds+1):
+    for outer in range(1, folds + 1):
         # The inner loop is for the inner fold or validation fold
-        for inner in range(1, opt.folds):
+        for inner in range(1, folds):
 
             # Inner fold is incremented by 1 to avoid having same inner and outer fold number for logging purposes
-            real_inner = inner +1 if outer <= inner else inner
+            real_inner = inner + 1 if outer <= inner else inner
             # Increment the counter
             counter += 1
 
             # Get the train, validation and test sets
             train_set, val_set, test_set = next(ncv_iterator)
             # Choose the model
-            model = choose_model(best_params, opt.tml_algorithm)
+            model = choose_model(best_params, tml_algorithm)
             # Fit the model
-            model.fit(train_set[descriptors], train_set['ddG'])
+            model.fit(train_set[descriptors], train_set["ddG"])
             # Predict the train set
             preds = model.predict(train_set[descriptors])
-            train_rmse = sqrt(mean_squared_error(train_set['ddG'], preds))
+            train_rmse = sqrt(mean_squared_error(train_set["ddG"], preds))
             # Predict the validation set
             preds = model.predict(val_set[descriptors])
-            val_rmse = sqrt(mean_squared_error(val_set['ddG'], preds))
+            val_rmse = sqrt(mean_squared_error(val_set["ddG"], preds))
             # Predict the test set
             preds = model.predict(test_set[descriptors])
-            test_rmse = sqrt(mean_squared_error(test_set['ddG'], preds))
+            test_rmse = sqrt(mean_squared_error(test_set["ddG"], preds))
 
-            print('Outer: {} | Inner: {} | Run {}/{} | Train RMSE {:.3f} kJ/mol | Val RMSE {:.3f} kJ/mol | Test RMSE {:.3f} kJ/mol'.\
-                  format(outer, real_inner, counter, TOT_RUNS, train_rmse, val_rmse, test_rmse) )
-            
+            print(
+                "Outer: {} | Inner: {} | Run {}/{} | Train RMSE {:.3f} kJ/mol | Val RMSE {:.3f} kJ/mol | Test RMSE {:.3f} kJ/mol".format(
+                    outer,
+                    real_inner,
+                    counter,
+                    TOT_RUNS,
+                    train_rmse,
+                    val_rmse,
+                    test_rmse,
+                )
+            )
+
             # Generate a report of the model performance
-            tml_report(log_dir=f"{opt.log_dir_results}/{opt.filename[:-4]}/learning/results_TML/{opt.tml_algorithm}/{opt.descriptors}/",
-                       data = (train_set, val_set, test_set),
-                       outer = outer,
-                       inner = real_inner,
-                       model = model,
-                       descriptors=descriptors
-                       )
-            
+            tml_report(
+                log_dir=log_results_dir,
+                data=(train_set, val_set, test_set),
+                outer=outer,
+                inner=real_inner,
+                model=model,
+                descriptors=descriptors,
+            )
+
             # Reset the variables of the training
             del model, train_set, val_set, test_set
-        
-        print('All runs for outer test fold {} completed'.format(outer))
-        print('Generating outer report')
+
+        print("All runs for outer test fold {} completed".format(outer))
+        print("Generating outer report")
 
         # Generate a report of the model performance for the outer/test fold
         network_outer_report(
-            log_dir=f"{opt.log_dir_results}/{opt.filename[:-4]}/learning/results_TML/{opt.tml_algorithm}/{opt.descriptors}/Fold_{outer}_test_set/",
+            log_dir=log_results_dir
+            / f"Fold_{outer}_test_set/",
             outer=outer,
-            folds=opt.folds,
+            folds=folds,
         )
 
-        print('---------------------------------')
-        
-    print('All runs completed')
+        print("---------------------------------")
+
+    print("All runs completed")
 
 
 if __name__ == "__main__":
